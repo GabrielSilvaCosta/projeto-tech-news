8936cc698737ba3e9b72128f4df5439ef14ac3c0
GabrielSilvaCosta <bob@>
2024-02-06 15:20:14 -0300

    requisito 10 feito

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,17 @@
-# Requisito 10
+from tech_news.database import db
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    categories_count = {}
+
+    for news in db["news"].find():
+        if news["category"] in categories_count:
+            categories_count[news["category"]] += 1
+        else:
+            categories_count[news["category"]] = 1
+
+    sorted_categories = sorted(
+        categories_count.items(), key=lambda x: (-x[1], x[0])
+    )[:5]
+
+    return [category[0] for category in sorted_categories]
