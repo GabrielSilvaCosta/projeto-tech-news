950442b8e4187d2811727c5c2d8376c2904991c7
GabrielSilvaCosta <bob@>
2024-02-06 15:40:30 -0300

    fazendo alguns testes

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,34 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from tech_news.analyzer.reading_plan import ReadingPlanService
 
 
 def test_reading_plan_group_news():
-    pass
+    result = ReadingPlanService.group_news_for_available_time(10)
+    assert result["readable"] == []
+    assert result["unreadable"] == [
+        ("Notícia 1", 1),
+    ]
+
+    result = ReadingPlanService.group_news_for_available_time(5)
+    assert result["readable"] == [
+        {"unfilled_time": 4, "chosen_news": [("Notícia 1", 1)]},
+    ]
+    assert result["unreadable"] == [
+        ("Notícia 2", 2),
+    ]
+
+    result = ReadingPlanService.group_news_for_available_time(3)
+    assert result["readable"] == [
+        {"unfilled_time": 2, "chosen_news": [("Notícia 1", 1)]},
+        {"unfilled_time": 1, "chosen_news": [("Notícia 2", 2)]},
+    ]
+    assert result["unreadable"] == [
+        ("Notícia 3", 3),
+    ]
+
+    result = ReadingPlanService.group_news_for_available_time(1)
+    assert result["readable"] == [
+        {"unfilled_time": 0, "chosen_news": [("Notícia 1", 1)]},
+        {"unfilled_time": 0, "chosen_news": [("Notícia 2", 2)]},
+        {"unfilled_time": 0, "chosen_news": [("Notícia 3", 3)]},
+    ]
+    assert result["unreadable"] == []
