c487342ddfb1b299e0162878112f108a12b31651
GabrielSilvaCosta <bob@>
2024-02-06 09:31:02 -0300

    requisito 7 feito

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,17 @@
+from tech_news.database import db
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    title_lower = title.lower()
+    result = []
+
+    for news in db["news"].find():
+        news_title_lower = news["title"].lower()
+        if title_lower in news_title_lower:
+            result.append((news["title"], news["url"]))
+
+    return result
 
 
 # Requisito 8
